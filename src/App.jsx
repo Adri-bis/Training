973f9c8932146a551e0ea3d20b8e92f078025157
Adri-bis <adrien.ma.16@hotmail.fr>
2024-04-15 11:29:36 +0200

    Creation due l'appli react

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import Algo from "./component/Algo";
+
+function App() {
+  return <Algo />;
+}
+
+export default App;
---
+++ src/component/Algo.js
@@ -0,0 +1,60 @@
+function Algo() {
+  const text = "AzeRtY";
+  const minus = text.toLocaleLowerCase();
+  const modif = minus.split("");
+  let essai = [];
+
+  for (let i = 0; i < modif.length; i++) {
+    let essai2 = [];
+    for (let y = 0; y <= i; y++) {
+      essai2.push(modif[i]);
+    }
+    essai.push(essai2);
+  }
+
+  console.log(essai);
+  let essay = [];
+
+  for (let i = 0; i < essai.length; i++) {
+    essay.push(essai[i].join(""));
+  }
+  console.log(essay);
+  let finish = [];
+  for (let i = 0; i < essay.length; i++) {
+    const result = essay[i].charAt(0).toUpperCase() + essay[i].slice(1);
+    finish.push(result);
+  }
+  console.log(finish);
+
+  const oka = finish.join("-");
+
+  console.log(oka);
+  // const texte = "je m'apelle adrien";
+  // const modif = texte.split(" ");
+  // console.log(modif);
+  // let finish = [];
+  // for (let i = 0; i < modif.length; i++) {
+  //   const result = modif[i].charAt(0).toUpperCase() + modif[i].slice(1);
+  //   finish.push(result);
+  // }
+  // let final = finish.join(" ");
+  // console.log(final);
+  // for (let i = 0; i < modif.length; i += 3) {}
+
+  // const s = "test";
+  // const t = s.split("");
+  // const Le = t.length;
+  // const Ml = Le % 2;
+  // let result = [];
+  // const id = Le / 2;
+  // if (Ml === 0) {
+  //   result.push(t[id - 1]) + result.push(t[id]);
+  // }
+
+  // const FR = result.join("");
+  // console.log(FR);
+
+  return finish;
+}
+
+export default Algo;
